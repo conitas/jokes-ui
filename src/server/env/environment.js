3244e1e12f221c846f2aad3107e8d26f7e8a7939
Ghenadii Batalski <ghenadii.batalski@itas-gmbh.de>
2018-06-05 11:32:59 +0000

    Docker image build introduced

--- src/server/env/environment.js
+++ src/server/env/environment.js
@@ -1,9 +1,11 @@
 const cosmosPort = process.env.APPSETTING_cosmosPort; // replace with your port
 const dbName = process.env.APPSETTING_dbName;
 const key = process.env.APPSETTING_key;
+const useProxy = process.env.APPSETTINGS_useProxy;
 
 module.exports = {
   dbName,
   key,
-  cosmosPort
+  cosmosPort,
+  useProxy
 };
